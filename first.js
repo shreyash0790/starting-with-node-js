66ef6ceea87b247b148c21061add30e78569b2f1
Shreyaskar <shreyash0790@gmail.com>
2023-07-04 03:32:37 +0530

    cleaning the code

--- first.js
+++ first.js
@@ -1,40 +1,7 @@
 const http = require('http');
-const fs=require('fs');
+const routes=require('./routes');
 
-const server = http.createServer((req, res) => {
-  const url = req.url;
-  const method=req.method;
-
-  if (url === '/') {
-   fs.readFile('message.text', {encoding:'utf-8'}, (err,data)=>{
-    if(err){
-      console.log(err)
-    }
-    else{
-    res.write('<html>');
-    res.write('<head></title>Enter message</title><head>');
-    res.write('<br>')
-    res.write(`<body>${data}</body>`)
-    res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
-    res.write('</html>');
- return  res.end();
-    }
-   })
-}
-else if(url==='/message'&& method==='POST'){
-const body=[]
-req.on('data', (chunk)=>{
-  body.push(chunk)
-})
-return req.on('end',()=>{
-  const parseBody=Buffer.concat(body).toString();
-  const message=parseBody.split('=')[1];
-  fs.writeFile('message.text', message,(err)=>{
-    res.statusCode=302;
-res.setHeader('Location','/');
-return res.end();
-  });
-})}});
+const server = http.createServer(routes)
 
 server.listen(4000, () => {
   console.log('Server started on port 4000');
