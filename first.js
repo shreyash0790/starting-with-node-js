ea5cafcf6c8c047b65b12ec9a566b6041337697c
Shreyaskar <shreyash0790@gmail.com>
2023-07-03 23:37:00 +0530

    chat app demo

--- first.js
+++ first.js
@@ -1,20 +1,40 @@
 const http = require('http');
+const fs=require('fs');
 
 const server = http.createServer((req, res) => {
   const url = req.url;
+  const method=req.method;
 
-  if (url === '/home') {
-    res.setHeader('Content-Type', 'text/html');
-    res.write('Welcome home');
-  } else if (url === '/about') {
-    res.setHeader('Content-Type', 'text/html');
-    res.write('Welcome to About Us page');
-  } else if (url === '/node') {
-    res.setHeader('Content-Type', 'text/html');
-    res.write('Welcome to my Node Js project');
-  } 
-  res.end();
-});
+  if (url === '/') {
+   fs.readFile('message.text', {encoding:'utf-8'}, (err,data)=>{
+    if(err){
+      console.log(err)
+    }
+    else{
+    res.write('<html>');
+    res.write('<head></title>Enter message</title><head>');
+    res.write('<br>')
+    res.write(`<body>${data}</body>`)
+    res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+    res.write('</html>');
+ return  res.end();
+    }
+   })
+}
+else if(url==='/message'&& method==='POST'){
+const body=[]
+req.on('data', (chunk)=>{
+  body.push(chunk)
+})
+return req.on('end',()=>{
+  const parseBody=Buffer.concat(body).toString();
+  const message=parseBody.split('=')[1];
+  fs.writeFile('message.text', message,(err)=>{
+    res.statusCode=302;
+res.setHeader('Location','/');
+return res.end();
+  });
+})}});
 
 server.listen(4000, () => {
   console.log('Server started on port 4000');
