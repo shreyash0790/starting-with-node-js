8dce6445548d35f10680338141da0101b9c7101a
Shreyaskar <shreyash0790@gmail.com>
2023-07-03 20:10:47 +0530

    request/response

--- first.js
+++ first.js
@@ -1,8 +1,21 @@
 const http = require('http');
 
-const server=http.createServer((req, res) => {
-  
-  console.log('Hello, World!');
+const server = http.createServer((req, res) => {
+  const url = req.url;
+
+  if (url === '/home') {
+    res.setHeader('Content-Type', 'text/html');
+    res.write('Welcome home');
+  } else if (url === '/about') {
+    res.setHeader('Content-Type', 'text/html');
+    res.write('Welcome to About Us page');
+  } else if (url === '/node') {
+    res.setHeader('Content-Type', 'text/html');
+    res.write('Welcome to my Node Js project');
+  } 
+  res.end();
 });
 
-server.listen(4000);
+server.listen(4000, () => {
+  console.log('Server started on port 4000');
+});
