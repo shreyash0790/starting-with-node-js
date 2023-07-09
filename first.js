f5f84c89c0d8560bde09676064ee2ae3ca8f7169
Shreyaskar <shreyash0790@gmail.com>
2023-07-09 08:32:17 +0530

    group chat app

--- first.js
+++ first.js
@@ -4,13 +4,11 @@ const express=require('express');
 const app=express();
 const adminroutes=require('./routes/admin')
 
-const shoproutes=require('./routes/shop');
-
 
 app.use(express.urlencoded({extended:false}));
 
-app.use('/admin',adminroutes);
-app.use(shoproutes);
+app.use(adminroutes);
+
 
 app.use((req,res,next)=>{
   res.status(404).send('<h1>page not found </h1>')
--- routes/admin.js
+++ routes/admin.js
@@ -1,14 +1,40 @@
 const express = require('express');
-
+const fs=require('fs');
 const router = express.Router();
 
-router.get('/add-prod', (req, res, next) => {
-    res.send('<form action="/admin/product" method="POST"><input type="text" name="title"><input type="text" name="size"><button type="submit">add product</button></form>')
+router.get('/login', (req, res, next) => {
+    res.send(`<form onsubmit="localStorage.setItem('username', document.getElementById('username').value)" action="/mess" method="POST">
+	<input id="username" type="text" name="username"><br>
+	<button type="submit">LOGIN</button></form>`);
 });
 
-router.post('/product', (req, res, next) => {
+
+
+router.post('/mess', (req, res, next) => {
     console.log(req.body);
+    if(req.body.messages){
+    fs.appendFile("user.txt",`${req.body.username}:${req.body.messages}\n`,(err)=>err?console.log(err):res.redirect('/'))
+    }
+    else{
     res.redirect('/');
+    }
 });
+
+router.get('/',(req,res,next)=>{
+    fs.readFile('user.txt',(err,data)=>{
+if(err){
+    console.log(err);
+    data="No chat exists"
+}
+res.send(`
+    ${data}<form action="/mess" method="POST" onsubmit="document.getElementById('username').value = localStorage.getItem('username')">
+        <input type="text" id="messages" name="messages">
+        <input type="hidden" id="username" name="username"><br>
+        <button type="submit">SEND</button>
+    </form>
+`);
+ 
+  });
+})
 
 module.exports=router;
